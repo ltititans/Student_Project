dcd0b776bedea0f3b82d54c507fc1723d4ba5734
ltititans <sindhura.shetty@yahoo.com>
2019-11-06 05:20:18 -0800

    initial

--- Hibernate_Project2/src/main/java/com/lti/hib_ex/Hibernate_Project2/App.java
+++ Hibernate_Project2/src/main/java/com/lti/hib_ex/Hibernate_Project2/App.java
@@ -1,5 +1,7 @@
 package com.lti.hib_ex.Hibernate_Project2;
 
+import java.util.Scanner;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
@@ -20,11 +22,27 @@ public class App
         SessionFactory factory=configuration.buildSessionFactory(builder.build());
         Session session=factory.openSession();
         Student student=new Student();
-        student.setRollno(1);
-        student.setName("A");
-        student.setAddress("Africa");
-        session.beginTransaction();
-        session.update(student);
-        session.getTransaction().commit();
+        Scanner ob=new Scanner(System.in);
+      System.out.println("Enter your choice:");
+      int n=ob.nextInt();
+      if(n==1)
+      {
+    	  student.setName("ABD");
+          student.setAddress("Africa");
+          session.beginTransaction();
+          session.save(student);
+          session.getTransaction().commit();
+      }
+      else
+    	  if(n==2)
+    	  {		
+    		  student.setRollno(1);
+    		  student.setName("MSD");
+              student.setAddress("India");
+              session.beginTransaction();
+              session.update(student);
+              session.getTransaction().commit();
+    	  }
+        
     }
 }
